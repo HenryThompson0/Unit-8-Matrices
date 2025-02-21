933b09d9294decf4035abd83b671240018b0d242
HenryThompson0 <sayjokesphotos@gmail.com>
2025-02-21 09:16:08 -0500

    Update Biggest.java

--- Biggest.java
+++ Biggest.java
@@ -6,6 +6,20 @@ public class Biggest
 {
     public static int getBig(int[][] m)
     {
-		return -1;
+        int rows = m.length;
+        int currentMax = Integer.MIN_VALUE;
+
+        for(int i = 0; i<rows; i++)
+        {
+            for(int j = 0; j<m[i].length; j++)
+            {
+                if (m[i][j]>currentMax)
+                {
+                    currentMax=m[i][j];
+                }
+            }
+        }
+
+		return currentMax;
     }
 }
