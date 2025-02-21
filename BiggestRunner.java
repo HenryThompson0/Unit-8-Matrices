5214e40e07f4d7659c795e755b8cc4d1fb32634f
HenryThompson0 <sayjokesphotos@gmail.com>
2025-02-21 09:15:49 -0500

    Update BiggestRunner.java

--- BiggestRunner.java
+++ BiggestRunner.java
@@ -6,12 +6,11 @@ public class BiggestRunner
 {
 	public static void main( String args[] ) throws Exception
 	{
-		int[][] f = {{1,2,3},{5,5,5,5}};
+		int[][] f = {{1,2,3},{5,8,5,5}};
 		System.out.println( "The biggest number is :: " + Biggest.getBig( f ) );
-		
+
 		//add more code and test cases		
 	}
 }
-
 
 
